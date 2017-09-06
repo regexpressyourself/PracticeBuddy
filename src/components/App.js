050072da344eb8131df00e7b646a5fadabd87484
Sam <samuel.messina@siu.edu>
2017-09-06 10:55:36 -0500

    Move all data to data.js
    kindof works like a database I guess

--- src/components/App.js
+++ src/components/App.js
@@ -1,75 +1,26 @@
 import React, { Component } from 'react';
 import logo from '../statics/logo.svg';
 import '../styles/App.css';
+import app_data from '../config/data.js';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            modes: [
-                {
-                    name: "Lydian",
-                    notes: ["1", "2",  "3",  "4+", "5",  "6",  "7"]
-                },
-                {
-                    name: "Ionian",
-                    notes: ["1", "2",  "3",  "4",  "5",  "6",  "7"]
-                },
-                {
-                    name: "Dorian",
-                    notes: ["1", "2",  "3-", "4",  "5",  "6",  "7-"]
-                },
-                {
-                    name: "Mixolydian",
-                    notes: ["1", "2",  "3",  "4",  "5",  "6",  "7-"]
-                },
-                {
-                    name: "Aeolian",
-                    notes: ["1", "2",  "3-", "4",  "5",  "6-", "7-"]
-                },
-                {
-                    name: "Phrygian",
-                    notes: ["1", "2-", "3-", "4",  "5",  "6-", "7-"]
-                },
-                {
-                    name: "Locrian",
-                    notes: ["1", "2-", "3-", "4",  "5-", "6-", "7-"]
-                }
-            ]
+            modes:                  app_data.modes,
+            mode_to_chromatic:      app_data.mode_to_chromatic,
+            sharp_based_chromatic:  app_data.sharp_based_chromatic,
+            flat_based_chromatic:   app_data.flat_based_chromatic
         }
     } 
     componentWillMount() {
-        let i = 0;
-        let modes = this.state.modes.map((mode) => {
-            let note_html = [];
-
-            mode["notes"].forEach((note) => {
-                note_html.push(<td key={i++}>{note}</td>);
-            });
-
-            let return_html = (
-                <tr key={i++}>
-                    <td>{mode["name"]}</td>
-                    {note_html}
-                </tr>
-            );
-            return return_html;
-        });
-
-        this.setState({
-            modes: modes
-        });
     }
     render() {
         return (
             <div>
-                <table id="mode-table">
-                    <tbody>
-                    {this.state.modes}
-                    </tbody>
-                </table>
+                {JSON.stringify(this.state.modes)}
             </div>
-            );
+        );
     }
 }
 
