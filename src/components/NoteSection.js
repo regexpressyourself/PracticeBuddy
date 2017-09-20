5cbcbca77f4c8fbd22bb46f78511d4183c41eaca
Sam <samuel.messina@siu.edu>
2017-09-20 11:54:04 -0500

    start work on changing starting pitch

--- src/components/NoteSection.js
+++ src/components/NoteSection.js
@@ -5,34 +5,74 @@ class NoteSection extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            notes:"",
-            intervals:""
+            intervals_array: props.intervals,
+            notes_array: props.notes,
+            sharp_based_chromatic: props.sharp_based_chromatic,
+            flat_based_chromatic: props.flat_based_chromatic,
+            offset: props.offset
         };
+        this.generateNotes = this.generateNotes.bind(this);
     }
     componentWillReceiveProps(nextProps) {
         let notes = [];
         let intervals = [];
-        for (let i = 0; i < nextProps.notes.length; i++) {
-            notes.push(
-                <span key={i} className={"note-cell note-cell"+i}>
-                    {nextProps.notes[i]}
+        let i = 0;
+        notes = nextProps.notes.map((note) => {
+            note = note + this.state.offset;
+            return (
+                <span key={i++} className={"note-cell note-cell"+i}>
+                    {this.state.sharp_based_chromatic[note]}
                 </span>
             );
-        }
-            console.log(nextProps);
-        for (let i = 0; i < nextProps.intervals.length; i++) {
-            intervals.push(
-                <span key={i} className={" interval-cell interval-cell"+i}>
-                    {nextProps.intervals[i]}
+        });
+        intervals = nextProps.intervals.map((interval) => {
+            return (
+                <span key={i} 
+                    className={" interval-cell interval-cell"+i++}>
+                    {interval}
                 </span>
             );
-        }
-    this.setState({
-        intervals: intervals,
-        notes: notes
+        });
+        this.setState({
+            intervals: intervals,
+            notes: notes,
+            notes_array: nextProps.notes,
+            intervals_array: nextProps.intervals,
+            offset: nextProps.offset
+        }, this.generateNotes());
+    }
 
-    });
+    componentWillMount() {
+        this.generateNotes();
+    }
 
+    generateNotes() {
+        let notes = [];
+        let intervals = [];
+        let i = 0;
+        notes = this.state.notes_array.map((note) => {
+            note = (note + this.state.offset) % 12;
+            console.log("note: " + note);
+            console.log("converted note: " + this.state.sharp_based_chromatic[note]);
+            return (
+                <span key={i++} className={"note-cell note-cell"+i}>
+                    {this.state.sharp_based_chromatic[note]}
+                </span>
+            );
+        });
+        intervals = this.state.intervals_array.map((interval) => {
+            return (
+                <span key={i} 
+                    className={" interval-cell interval-cell"+i++}>
+                    {interval}
+                </span>
+            );
+        });
+        console.log(notes);
+        this.setState({
+            intervals: intervals,
+            notes: notes
+        });
     }
     render() {
         return (
