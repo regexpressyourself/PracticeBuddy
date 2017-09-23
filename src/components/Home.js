1e1f293210ad97a91b10fa1bcc803abcb1f3e9cb
Sam <samuel.messina@siu.edu>
2017-09-23 15:15:05 -0500

    add logo to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,15 @@
 import React, { Component} from 'react';
+import logo_image from '../statics/img/practicebuddy_logo.png';
 
 class Home extends Component {
 
     render() {
         return (
             <div id="home">
+                <img id="homepage-image" src={logo_image} />
+                <p>
+                    Practice Buddy is a quick and easy way to reference musical modes, chords, and charts. 
+                </p>
             </div>
         );
     }
