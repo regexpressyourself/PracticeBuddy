5dea0b53e38640640b9731845312a0056119755d
Sam <samuel.messina@siu.edu>
2017-09-23 14:13:03 -0500

    Move route declarations to app component
    this is more in line with React v 4 methods

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
-import MainWrapper from './MainWrapper';
+import '../styles/index.css';
+import Nav from './Nav';
+import Home from '../components/Home';
+import Modes from '../components/Modes';
+import Chords from '../components/Chords';
+import Charts from '../components/Charts';
+import About from '../components/About';
+import {
+    BrowserRouter as Router,
+    Route
+} from 'react-router-dom'
+
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        console.log(props);
+    }
     componentWillMount() {
     }
     render() {
         return (
-                <MainWrapper />
+            <Router>
+                <div id="main-container">
+                    <Nav />
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/modes" component={Modes}/>
+                    <Route path="/chords" component={Chords}/>
+                    <Route path="/charts" component={Charts}/>
+                    <Route path="/about" component={About}/>
+                </div>
+            </Router>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
-import routes from './config/routes';
+import App from './components/App';
 
 ReactDOM.render(
-  routes,
-  document.getElementById('index')
+    <App />,
+    document.getElementById('index')
 );
