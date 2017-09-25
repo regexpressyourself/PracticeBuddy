b63f7f0c50a1f473fab59ce7695c862c6c8ee281
Sam <samuel.messina@siu.edu>
2017-09-25 11:49:14 -0500

    change homepage image to background image

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,8 @@ class Home extends Component {
     render() {
         return (
             <div id="home">
-                <img id="homepage-image" src={logo_image} />
+            <div id="homepage-image">
+            </div>
                 <p>
                     Practice Buddy is a quick and easy way to reference musical modes, chords, and charts. 
                 </p>
