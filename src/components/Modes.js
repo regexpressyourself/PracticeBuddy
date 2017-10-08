8f74ebf5aecf8cd4913ebccf336cb9edc1f31ae1
Sam <samuel.messina@siu.edu>
2017-10-08 13:11:05 -0500

    add working note chooser

--- src/components/Modes.js
+++ src/components/Modes.js
@@ -14,8 +14,10 @@ class Modes extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            app_data:               app_data,
             modes:                  app_data.modes,
             mode_to_chromatic:      app_data.mode_to_chromatic,
+            current_chromatic:      app_data.sharp_based_chromatic,
             sharp_based_chromatic:  app_data.sharp_based_chromatic,
             flat_based_chromatic:   app_data.flat_based_chromatic,
             mode_list_display: "none",
@@ -29,10 +31,10 @@ class Modes extends Component {
         };
         this.incrementMode= this.incrementMode.bind(this);
         this.decrementMode= this.decrementMode.bind(this);
-        this.changeMode= this.changeMode.bind(this);
         this.changePitch= this.changePitch.bind(this);
         this.handleKeypress= this.handleKeypress.bind(this);
         this.changeToMode= this.changeToMode.bind(this);
+        this.changeToNote= this.changeToNote.bind(this);
     }
 
     componentDidMount() {
@@ -100,11 +102,6 @@ class Modes extends Component {
         });
     }
 
-    changeMode() {
-        this.setState({
-            current_offset: (this.state.current_offset + 1)
-        });
-    }
     changePitch() {
         this.setState({
             current_offset: (this.state.current_offset + 11) 
@@ -131,13 +128,51 @@ class Modes extends Component {
             current_mode: current_mode
         }, this.unsetModeChooser());
     }
+    unsetNoteChooser() {
+        this.setState({
+            note_list_display: "none"
+        });
+    }
+    setNoteChooser() {
+        this.setState({
+            note_list_display: "flex"
+        });
+    }
+
+    changeToNote(new_note) {
+        let i = 0;
+        let chromatic_array;
+        if (new_note.indexOf("b") !== -1) {
+            chromatic_array = this.state.flat_based_chromatic;
+        }
+        else if (new_note.indexOf("#") !== -1) {
+            chromatic_array = this.state.sharp_based_chromatic;
+        }
+        else {
+            // TODO figure out sharp vs flat rule on 
+            // natural starting pitches
+            chromatic_array = this.state.flat_based_chromatic;
+        }
+        console.log("new_note: "+new_note);
+        console.log("chrom_array: "+chromatic_array);
+
+        while (chromatic_array[i] !== new_note && i < 20) {
+            i++;
+        }
+        this.setState({
+            current_offset: i,
+            current_chromatic: chromatic_array
+        }, this.unsetNoteChooser());
+    }
 
     render() {
         return (
             <div id="mode-container">
                 <ModeChooser changeToMode={this.changeToMode} 
                     mode_list_display={this.state.mode_list_display} />
-                <NoteChooser note_list_display={this.state.note_list_display} />
+                <NoteChooser changeToNote={this.changeToNote}
+                    note_list_display={this.state.note_list_display} 
+                    cool_notes={this.state.app_data.cool_notes} />
 
                 {/* Mode and Blurb */}
 
@@ -154,8 +189,7 @@ class Modes extends Component {
                     intervals={this.state.interval_array[this.state.current_mode]}
                     offset={this.state.current_offset}
                     mode_to_chromatic={this.state.mode_to_chromatic}
-                    sharp_based_chromatic={this.state.sharp_based_chromatic}
-                    flat_based_chromatic={this.state.flat_based_chromatic}
+                    chromatic={this.state.current_chromatic}
                 />
 
                 { /* Buttons */ }
@@ -181,7 +215,7 @@ class Modes extends Component {
                     </button>
                 </div>
                 <div  className="center-grid-area pitch-btn-area">
-                    <button onClick={this.changePitch} className="pitch-btn btn">
+                    <button onClick={() => this.setNoteChooser()} className="pitch-btn btn">
                     <img className="fa" src={music} alt="" />
                         <span className="btn-text">
                         Starting Pitch 
--- src/components/NoteChooser.js
+++ src/components/NoteChooser.js
@@ -5,7 +5,11 @@ class NoteChooser extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            note_list_display: this.props.note_list_display
+            note_list_display: this.props.note_list_display,
+            note_list_class: "on",
+            cool_notes: props.cool_notes,
+            flat_sharp_list_class: "off",
+            second_screen: ""
         };
     }
     componentWillReceiveProps(nextProps) {
@@ -13,45 +17,107 @@ class NoteChooser extends Component {
             note_list_display: nextProps.note_list_display
         });
     }
+    storeNoteName(note_name) {
+        let is_flat = this.state.cool_notes[note_name].indexOf("flat") !== -1;
+        let is_sharp = this.state.cool_notes[note_name].indexOf("sharp") !== -1;
+        let second_screen = [];
+        let note_reminder = ( <span key="1" id="note-reminder">
+                                <span>
+                                    {note_name}
+                                </span>
+                            </span>);
+        let flat_span = ( <span key="2" onClick={() => this.storeFlatSharp('b')}>
+            <span>
+            b
+            </span>
+            </span>);
+        let sharp_span = ( <span key="3" onClick={() => this.storeFlatSharp('#')}>
+            <span>
+            #
+            </span>
+            </span>);
+        let natural_span = ( <span key="4" onClick={() => this.storeFlatSharp('natural')}>
+            <span>
+            natural
+            </span>
+            </span>);
+
+        if (is_flat && is_sharp) {
+            second_screen = [ note_reminder, sharp_span, natural_span, flat_span ]
+
+        }
+        else if (is_flat) {
+            second_screen = [ note_reminder, natural_span, flat_span ]
+        }
+        else {
+            second_screen = [ note_reminder, sharp_span, natural_span ]
+        }
+
+        this.setState({
+            second_screen: second_screen,
+            note_name: note_name,
+            note_list_class: "off",
+            flat_sharp_list_class: "on",
+        });
+    }
+    storeFlatSharp(flat_sharp) {
+        this.setState({
+            second_screen: "",
+            note_list_class: "on",
+            flat_sharp_list_class: "off",
+        }, this.sendNoteToParentComponent(flat_sharp));
+    }
+    sendNoteToParentComponent(flat_sharp_param) {
+        let note = "";
+        if (flat_sharp_param === 'natural') {
+            note = this.state.note_name;
+        }
+        else {
+            note = this.state.note_name + flat_sharp_param;
+        }
+        this.props.changeToNote(note);
+    }
 
     render() {
         return (
-            <div style={{display: this.state.note_list_display}} className="chooser-list change-mode-list">
-                <li onClick={() => this.props.changeToMode("Ionian")}>
-                    <p>
-                        Ionian
-                    </p>
-                </li>
-                <li onClick={() => this.props.changeToMode("Dorian")}>
-                    <p>
-                        Dorian
-                    </p>
-                </li>
-                <li onClick={() => this.props.changeToMode("Phrygian")}>
-                    <p>
-                        Phrygian
-                    </p>
-                </li>
-                <li onClick={() => this.props.changeToMode("Lydian")}>
-                    <p>
-                        Lydian
-                    </p>
-                </li>
-                <li onClick={() => this.props.changeToMode("Mixolydian")}>
-                    <p>
-                        Mixolydian
-                    </p>
-                </li>
-                <li onClick={() => this.props.changeToMode("Aeolian")}>
-                    <p>
-                        Aeolian
-                    </p>
-                </li>
-                <li onClick={() => this.props.changeToMode("Locrian")} style={{ border:"none" }}>
-                    <p>
-                        Locrian
-                    </p>
-                </li>
+            <div style={{display: this.state.note_list_display}} 
+                className="chooser-list change-note-list">
+                <div className={"note-chooser-grid "+this.state.note_list_class}>
+                    <span id="note1"
+                        onClick={() => this.storeNoteName('C')}>
+                        <span>C</span>
+                    </span>
+                    <span id="note2"
+                        onClick={() => this.storeNoteName('D')}>
+                        <span>D</span>
+                    </span>
+                    <span id="note3"
+                        onClick={() => this.storeNoteName('E')}>
+                        <span>E</span>
+                    </span>
+                    <span id="note4"
+                        onClick={() => this.storeNoteName('F')}>
+                        <span>F</span>
+                    </span>
+                    <span id="note5"
+                        onClick={() => this.storeNoteName('G')}>
+                        <span>G</span>
+                    </span>
+                    <span id="note6"
+                        onClick={() => this.storeNoteName('A')}>
+                        <span>A</span>
+                    </span>
+                    <span id="note7"
+                        onClick={() => this.storeNoteName('B')}>
+                        <span>B</span>
+                    </span>
+            </div>
+            <div className={"flat-sharp-chooser-grid " + this.state.flat_sharp_list_class}
+            style={{
+                gridTemplateRows:"1fr repeat("+(this.state.second_screen.length-1)+", 2fr)"
+            }} >
+            {this.state.second_screen}
+            </div>
             </div>
         );
     }
