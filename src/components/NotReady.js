7160c82191f488046a62d4b575205883d5114236
Sam <samuel.messina@siu.edu>
2017-10-08 14:24:59 -0500

    update the not ready component to actually display

--- src/components/NotReady.js
+++ src/components/NotReady.js
@@ -2,13 +2,22 @@ import React, { Component} from 'react';
 import '../styles/NotReady.css';
 
 class NotReady extends Component {
+    constructor(props) {
+        super(props);
+    }
     render() {
         return (
             <div className="not-ready">
                 This page is not ready yet.
-
+                <p>
+                    {this.props.blurb}
+                </p>
+                <button onClick={() => window.location.href=this.props.link} 
+                    className="github-link-btn btn">
+                    {this.props.button_text}
+                </button>
             </div>
-        );
+            );
     }
 }
 
