e3c54e90c7ba522365d0a46ef496325d4567dcc8
Sam <samuel.messina@siu.edu>
2017-10-08 14:25:42 -0500

    add not ready components to pages in development

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -1,13 +1,24 @@
 import React, { Component} from 'react';
+import NotReady from '../components/NotReady';
 //import styles from './Charts.css';
 
 class Charts extends Component {
     render() {
+        let data = {
+            blurb: `The Charts section of the site is still 
+            in development. If you'd like to help, feel free 
+            to check out the project's progress:`,
+            button_text:"Charts Project",
+            link:"https://github.com/regexpressyourself/PracticeBuddy/projects/3"
+
+        }
         return (
-            <div>
-                Charts coming soon.
+            <div id="chords-page">
+                <NotReady blurb={data.blurb} 
+                    button_text={data.button_text}
+                    link={data.link} />
             </div>
-        );
+            );
     }
 }
 
--- src/components/Chords.js
+++ src/components/Chords.js
@@ -1,13 +1,24 @@
 import React, { Component} from 'react';
+import NotReady from '../components/NotReady';
 //import styles from './Chords.css';
 
 class Chords extends Component {
     render() {
+        let data = {
+            blurb: `The Chords section of the site is still 
+            in development. If you'd like to help, feel free 
+            to check out the project's progress:`,
+            button_text:"Chords Project",
+            link:"https://github.com/regexpressyourself/PracticeBuddy/projects/2"
+
+        }
         return (
-            <div>
-                Chords coming soon.
+            <div id="chords-page">
+                <NotReady blurb={data.blurb} 
+                    button_text={data.button_text}
+                    link={data.link} />
             </div>
-        );
+            );
     }
 }
 
