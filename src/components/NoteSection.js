26cc76689f7156a2cc8750f016cf429f243d1e60
Sam <samuel.messina@siu.edu>
2017-10-08 13:11:22 -0500

    fix bug in offset definitions

--- src/components/NoteSection.js
+++ src/components/NoteSection.js
@@ -7,8 +7,7 @@ class NoteSection extends Component {
         this.state = {
             intervals_array: props.intervals,
             notes_array: props.notes,
-            flat_based_chromatic: props.flat_based_chromatic,
-            flat_based_chromatic: props.flat_based_chromatic,
+            chromatic: props.chromatic,
             offset: props.offset
         };
         this.generateNotes = this.generateNotes.bind(this);
@@ -17,52 +16,70 @@ class NoteSection extends Component {
         let notes = [];
         let intervals = [];
         let i = 0;
-        notes = nextProps.notes.map((note) => {
-            note = note + this.state.offset;
-            note = this.state.flat_based_chromatic[note];
-            if (note.includes("#")) {
-                note= <span>{note.replace("#","")}<span className="flat-sharp">#</span></span>
-            }
-            else if (note.includes("b")) {
-                note= <span>{note.replace("b","")}<span className="flat-sharp">b</span></span>
-            }
-            return (
-                <span key={i++} className={"note-cell note-cell"+i}>
-                    {note}
-                </span>
-            );
-        });
-        intervals = nextProps.intervals.map((interval) => {
-            if (interval.includes("-")) {
-                interval = <span>{interval.replace("-","")}<span className="flat-sharp">-</span></span>
-            }
-            return (
-                <span key={i} 
-                    className={" interval-cell interval-cell"+i++}>
-                    {interval}
-                </span>
-            );
-        });
+        let offset = nextProps.offset === undefined ? 
+             this.state.offset :nextProps.offset ;
+        let chromatic = nextProps.chromatic ? 
+            nextProps.chromatic : this.state.chromatic;
+        notes = this.state.notes;
+        if (nextProps.notes) {
+            notes = nextProps.notes.map((note) => {
+                if (note === undefined) { return; }
+                note = (note + offset) % 12;
+                note = chromatic[note];
+                if (note.includes("#")) {
+                    note= <span>{note.replace("#","")}
+                        <span className="flat-sharp">#</span>
+                        </span>
+                }
+                else if (note.includes("b")) {
+                    note= <span>{note.replace("b","")}
+                        <span className="flat-sharp">b</span>
+                        </span>
+                }
+                return (
+                    <span key={i++} className={"note-cell note-cell"+i}>
+                        {note}
+                    </span>
+                );
+            });
+        }
+        intervals=this.state.intervals;
+        if (nextProps.intervals) {
+            intervals = nextProps.intervals.map((interval) => {
+                if (!interval){ return; }
+                if (interval.includes("-")) {
+                    interval = <span>{interval.replace("-","")}<span className="flat-sharp">-</span></span>
+                }
+                return (
+                    <span key={i} 
+                        className={" interval-cell interval-cell"+i++}>
+                        {interval}
+                    </span>
+                );
+            });
+        }
         this.setState({
             intervals: intervals,
             notes: notes,
-            notes_array: nextProps.notes,
-            intervals_array: nextProps.intervals,
-            offset: nextProps.offset
-        }, this.generateNotes());
+            notes_array: nextProps.notes ? nextProps.notes : this.state.notes,
+            intervals_array: nextProps.intervals ? nextProps.intervals : this.state.intervals,
+            offset: nextProps.offset ? nextProps.offset : this.state.offset
+        }, this.generateNotes(nextProps.offset));
     }
 
     componentWillMount() {
         this.generateNotes();
     }
 
-    generateNotes() {
-        let notes = [];
+    generateNotes(offset) {
+        if (!offset) {offset=this.state.offset}
+        console.log("offset changed: " + offset);
+        let new_notes = [];
         let intervals = [];
         let i = 0;
-        notes = this.state.notes_array.map((note) => {
-            note = (note + this.state.offset) % 12;
-            note = this.state.flat_based_chromatic[note];
+        new_notes = this.state.notes_array.map((note) => {
+            note = (note + offset) % 12;
+            note = this.state.chromatic[note];
             return (
                 <span key={i++} className={"note-cell note-cell"+i}>
                     {note}
@@ -77,10 +94,9 @@ class NoteSection extends Component {
                 </span>
             );
         });
-        console.log(notes);
         this.setState({
             intervals: intervals,
-            notes: notes
+            notes: new_notes
         });
     }
     render() {
